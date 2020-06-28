3038eda901b2d4a823b3ae7631ce7f1ca8ad2c52
iamdavidzeng <15779712170@163.com>
2020-06-28 20:37:35 +0800

    article model

---
+++ routers/api/v1/article.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// GetArticle comment
+func GetArticle(c *gin.Context) {
+
+}
+
+// GetArticles c
+func GetArticles(c *gin.Context) {
+
+}
+
+// AddArticle c
+func AddArticle(c *gin.Context) {
+
+}
+
+// EditArticle c
+func EditArticle(c *gin.Context) {
+
+}
+
+// DeleteArticle c
+func DeleteArticle(c *gin.Context) {
+
+}
