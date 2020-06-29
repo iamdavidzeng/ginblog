565cdf50a881e0c8803df26f7fbb8c838b89908f
iamdavidzeng <15779712170@163.com>
2020-06-29 22:58:56 +0800

    article api

--- models/article.go
+++ models/article.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
 type Article struct {
 	Model
 
@@ -12,4 +18,16 @@ type Article struct {
 	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
 	State      int    `json:"state"`
+}
+
+func (article *Article) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (article *Article) BeforeUpdate(scope *grom.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
 }
