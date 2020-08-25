851a85133c9f08c065e7a82d801b6c1c66b9c244
iamdavidzeng <15779712170@163.com>
2020-08-25 16:10:26 +0800

    Use channel accept signal

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
 	"log"
-	"syscall"
-
-	"github.com/fvbock/endless"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	router := routers.InitRouter()
 
-	server := endless.NewServer(endPoint, routers.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err: %v", err)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
 }
